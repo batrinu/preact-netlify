469f2fae4aefbbfedfe36224cc5a1888dea236de
batrinu <batrinu@outlook.com>
2023-05-27 17:38:43 +0300

    overhaul

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -4,57 +4,37 @@ import style from "./style";
 import { usePrerenderData } from '@preact/prerender-data-provider';
 
 const Home = (props) => {
-	const [data, isLoading] = usePrerenderData(props);
-	console.log({data})
-	/**
-	 * Netlify CMS's accept invite link land on home page.
-	 * This redirection takes it to the right place(/admin).
-	 */
+  const [data, isLoading] = usePrerenderData(props);
 
-	useEffect(() => {
-		if (window !== undefined && window.location.href.includes('#invite_token')) {
-			const { href } = window.location;
-			window.location.href= `${href.substring(0, href.indexOf('#'))}admin${href.substring(href.indexOf('#'))}`;
-		}
-	},[]);
+  useEffect(() => {
+    if (window !== undefined && window.location.href.includes('#invite_token')) {
+      const { href } = window.location;
+      window.location.href= `${href.substring(0, href.indexOf('#'))}admin${href.substring(href.indexOf('#'))}`;
+    }
+  },[]);
 
-	return (
-		<div class={style.home}>
-			<div class={style.about}>
-				<div class={style.imageContainer}>
-					<div class={style.image} />
-				</div>
-				<div class={style.quote}>
-					<div class={style.details}>
-						Jane presents traditional spanish art with a modern twist. Her work is
-						excellent quality, her technique is brilliant and her love of the
-						subject matter shines through.
-					</div>
-					<div class={style.author}>- The Local Newspaper</div>
-				</div>
-			</div>
-			<div class={style.bio}>
-				<p class={style.bioleft}>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-					minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-					aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-					pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-					culpa qui officia deserunt mollit anim id est laborum.
-				</p>
-				<p class={style.bioright}>
-					Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-					accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-					ab illo inventore veritatis et quasi architecto beatae vitae dicta
-					sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-					aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-					qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-					dolorem ipsum quia dolor sit amet, consectetur.
-				</p>
-			</div>
-		</div>
-	);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!data || data.error) {
+    return <div>Error loading data</div>;
+  }
+
+  const { heroTitle, heroDescription, aboutTitle, aboutDescription } = data?.data?.edges?.[0]?.details ?? {};
+
+  return (
+	<div class={style.home} id="home">
+	  <section class={style.hero} id="hero">
+		<h1 class={style.heroTitle}>{heroTitle}</h1>
+		<p class={style.heroDescription}>{heroDescription}</p>
+	  </section>
+	  <section class={style.about} id="about">
+		<h2 class={style.aboutTitle}>{aboutTitle}</h2>
+		<p class={style.aboutDescription}>{aboutDescription}</p>
+	  </section>
+	</div>
+  );
 };
 
 export default Home;
